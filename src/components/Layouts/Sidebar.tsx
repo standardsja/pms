1f480f63f1bcaf0deea7e1b06f0e69cdd33815b5
Kymarley-Millwood <Kmillwood@bsj.org.jm>
2025-11-03 10:15:06 -0500

    Kymarley

--- src/components/Layouts/Sidebar.tsx
+++ src/components/Layouts/Sidebar.tsx
@@ -113,14 +113,14 @@ const Sidebar = () => {
                                 </NavLink>
                             </li>
 
-                            <li className="nav-item">
+                            {/* <li className="nav-item">
                                 <NavLink to="/procurement/quotes" className="group">
                                     <div className="flex items-center">
                                         <IconDollarSignCircle className="group-hover:!text-primary shrink-0" />
                                         <span className="ltr:pl-3 rtl:pr-3 text-black dark:text-[#506690] dark:group-hover:text-white-dark">Quotes</span>
                                     </div>
                                 </NavLink>
-                            </li>
+                            </li> */}
 
                             <li className="nav-item">
                                 <NavLink to="/procurement/evaluation" className="group">
