42b23199a10414b59d686a93018f39ff9930520a
Kymarley Millwood <Kmillwood@bsj.org.jm>
2025-11-03 12:44:12 -0500

    Kymarley

--- src/components/Layouts/Sidebar.tsx
+++ src/components/Layouts/Sidebar.tsx
@@ -93,6 +93,20 @@ const Sidebar = () => {
                         <ul className="relative font-semibold space-y-0.5 p-4 py-0">
                             <h2 className="py-3 px-7 flex items-center uppercase font-extrabold bg-white-light/30 dark:bg-dark dark:bg-opacity-[0.08] -mx-4 mb-1">
                                 <IconMinus className="w-4 h-5 flex-none hidden" />
+                                <span>USER</span>
+                            </h2>
+
+                            <li className="nav-item">
+                                <NavLink to="/apps/requests" className="group">
+                                    <div className="flex items-center">
+                                        <IconFile className="group-hover:!text-primary shrink-0" />
+                                        <span className="ltr:pl-3 rtl:pr-3 text-black dark:text-[#506690] dark:group-hover:text-white-dark">Requests</span>
+                                    </div>
+                                </NavLink>
+                            </li>
+
+                            <h2 className="py-3 px-7 flex items-center uppercase font-extrabold bg-white-light/30 dark:bg-dark dark:bg-opacity-[0.08] -mx-4 mb-1 mt-4">
+                                <IconMinus className="w-4 h-5 flex-none hidden" />
                                 <span>Procurement Officer</span>
                             </h2>
 
@@ -101,15 +115,6 @@ const Sidebar = () => {
                                     <div className="flex items-center">
                                         <IconMenuDashboard className="group-hover:!text-primary shrink-0" />
                                         <span className="ltr:pl-3 rtl:pr-3 text-black dark:text-[#506690] dark:group-hover:text-white-dark">Dashboard</span>
-                                    </div>
-                                </NavLink>
-                            </li>
-
-                            <li className="nav-item">
-                                <NavLink to="/apps/requests" className="group">
-                                    <div className="flex items-center">
-                                        <IconFile className="group-hover:!text-primary shrink-0" />
-                                        <span className="ltr:pl-3 rtl:pr-3 text-black dark:text-[#506690] dark:group-hover:text-white-dark">Requests</span>
                                     </div>
                                 </NavLink>
                             </li>
