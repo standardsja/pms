8c2f5b2e7218fcf7100df2032628be74d87cf840
Kymarley Millwood <Kmillwood@bsj.org.jm>
2025-11-07 09:09:27 -0500

    feat: Add user section and requests link to Sidebar for enhanced navigation

--- src/components/Layouts/Sidebar.tsx
+++ src/components/Layouts/Sidebar.tsx
@@ -215,6 +215,20 @@ const Sidebar = () => {
                                 <>
                             <h2 className="py-3 px-7 flex items-center uppercase font-extrabold bg-white-light/30 dark:bg-dark dark:bg-opacity-[0.08] -mx-4 mb-1">
                                 <IconMinus className="w-4 h-5 flex-none hidden" />
+                                <span>USER</span>
+                            </h2>
+
+                            <li className="nav-item">
+                                <NavLink to="/apps/requests" className="group">
+                                    <div className="flex items-center">
+                                        <IconFile className="group-hover:!text-primary shrink-0" />
+                                        <span className="ltr:pl-3 rtl:pr-3 text-black dark:text-[#506690] dark:group-hover:text-white-dark">Requests</span>
+                                    </div>
+                                </NavLink>
+                            </li>
+
+                            <h2 className="py-3 px-7 flex items-center uppercase font-extrabold bg-white-light/30 dark:bg-dark dark:bg-opacity-[0.08] -mx-4 mb-1 mt-4">
+                                <IconMinus className="w-4 h-5 flex-none hidden" />
                                 <span>Procurement Officer</span>
                             </h2>
 
