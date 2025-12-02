c27c493106ddd533e7f9b2d9d000d72e70745866
khansonbsj <khanson@bsj.org.jm>
2025-12-02 14:39:25 -0500

    fix(Evaluation): remove unnecessary closing tag in EvaluationDetail component

--- src/pages/Procurement/Evaluation/EvaluationDetail.tsx
+++ src/pages/Procurement/Evaluation/EvaluationDetail.tsx
@@ -144,7 +144,6 @@ const EvaluationDetail = () => {
                     setEvaluation(updated);
                 }}
             />
-                            )}
                         </div>
                     )}
 
